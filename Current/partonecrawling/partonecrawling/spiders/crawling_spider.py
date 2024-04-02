3b086071d06f8cde8957aa9f3b2b4a491720aaf8
Brenda Coronel <bcoronel@hawk.iit.edu>
2024-04-02 12:47:18 -0500

    Crawler gathers content from paragraphs scrapped

--- Current/partonecrawling/partonecrawling/spiders/crawling_spider.py
+++ Current/partonecrawling/partonecrawling/spiders/crawling_spider.py
@@ -1,11 +1,13 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
 class CrawlingSpider(CrawlSpider):
+
     name = "mycrawler"
-    allowed_domains = ["toscrape.com"]
-    start_urls = ["http://books.toscrape.com/"]
-    max_pages = 10
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Illinois_Institute_of_Technology"]
+    max_pages = 5
     max_depth = 5
 
     custom_settings = {
@@ -13,6 +15,19 @@ class CrawlingSpider(CrawlSpider):
         'DEPTH_LIMIT': max_depth,
     }
 
+    output_file = 'paragraphs.txt'  # Output file name
+
     rules = (
-        Rule(LinkExtractor(allow="catalogue/category")),
+        Rule(LinkExtractor(allow="wiki/"), callback='parse_page', follow=True),
     )
+
+    def parse_page(self, response):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        paragraphs = soup.find_all('p')
+        with open(self.output_file, 'a', encoding='utf-8') as f:
+            for paragraph in paragraphs:
+                f.write(paragraph.get_text() + '\n')
+        for paragraph in paragraphs:
+            yield {
+                "paragraph": paragraph.get_text()
+            }
