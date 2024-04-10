1730253cc6cb77b2b59d55495ed40617e9043e91
Brenda Coronel <bcoronel@hawk.iit.edu>
2024-04-10 12:44:07 -0500

    Indexed and query over titles

--- Current/partonecrawling/partonecrawling/spiders/crawling_spider.py
+++ Current/partonecrawling/partonecrawling/spiders/crawling_spider.py
@@ -8,35 +8,30 @@ class CrawlingSpider(CrawlSpider):
     name = "mycrawler"
     allowed_domains = ["en.wikipedia.org"]
     start_urls = ["https://en.wikipedia.org/wiki/Illinois_Institute_of_Technology"]
-    max_pages = 5
-    max_depth = 5
+    max_pages = 100
+    max_depth = 100
 
     custom_settings = {
         'CLOSESPIDER_PAGECOUNT': max_pages,
         'DEPTH_LIMIT': max_depth,
     }
 
-    output_file = 'raw_paragraphs.txt'  # Output file name
+    output_file = 'raw_titles.txt'
 
     rules = (
         Rule(LinkExtractor(allow="wiki/"), callback='parse_page', follow=True),
     )
-
+    
+    # scraping titles of the wiki pages
     def parse_page(self, response):
-        url = response.url
-        paragraphs = self.extract_paragraphs(url)
+        title = self.extract_title(response)
         with open(self.output_file, 'a', encoding='utf-8') as f:
-            for paragraph in paragraphs:
-                f.write(paragraph + '\n')
-        for paragraph in paragraphs:
-            yield {
-                "paragraph": paragraph
-            }
+            f.write(title + '\n')
+        yield {
+            "title": title
+        }
 
-    def extract_paragraphs(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        content = soup.find(id="mw-content-text")
-        paragraphs = content.find_all('p')
-        text = '\n'.join([paragraph.get_text() for paragraph in paragraphs])
-        return text.split('\n')
+    def extract_title(self, response):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('h1', id='firstHeading').get_text()
+        return title
--- Current/parttwoindexer/pickle_indexer.py
+++ Current/parttwoindexer/pickle_indexer.py
@@ -7,55 +7,36 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Path to the input and output files
-input_file_path = 'raw_paragraphs.txt'
-output_file_path = 'clean_paragraphs.txt'
+input_file_path = 'raw_titles.txt'  
+output_file_path = 'clean_titles.txt' 
 
-# Regular expression to remove extra spaces
-pattern = re.compile(r'\s+')
+# load docs from cleaned_titles
+with open(input_file_path, 'r', encoding='utf-8') as file:
+    titles = file.readlines()
 
-with open(input_file_path, 'r', encoding='utf-8') as input_file, \
-        open(output_file_path, 'w', encoding='utf-8') as output_file:
-    for line in input_file:
-        # Remove extra spaces
-        cleaned_line = re.sub(pattern, ' ', line.strip())
-        if cleaned_line:  # Check if the line is not empty after cleaning
-            output_file.write(cleaned_line + '\n')
-
-
-# Load documents from output_file.txt
-with open('clean_paragraphs.txt', 'r', encoding='utf-8') as file:
-    documents = file.readlines()
-
-# Calculate TF-IDF scores
+# tf-idf vector
 vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(documents)
+tfidf_matrix = vectorizer.fit_transform(titles)
 
-# Construct inverted index
+# creating inverted index
 terms = vectorizer.get_feature_names_out()
 inverted_index = {}
 for i, term in enumerate(terms):
     inverted_index[term] = [(j, tfidf_matrix[j, i]) for j in range(tfidf_matrix.shape[0]) if tfidf_matrix[j, i] > 0]
 
-# Save the inverted index to a pickle file
-with open('inverted_index.pkl', 'wb') as file:
+# inverted index saved to pickle file
+with open('inverted_index_titles.pkl', 'wb') as file:  
     pickle.dump(inverted_index, file)
 
-
-# Calculate cosine similarity between query and documents
+# cosine similairty bw query and doc
 def search(query):
     query_vector = vectorizer.transform([query])
-    print("Query Vector:")
-    print(query_vector)
-    
     cosine_similarities = cosine_similarity(query_vector, tfidf_matrix)
-    print("Cosine Similarities Matrix:")
-    print(cosine_similarities)
-    
-    results = [(documents[i], cosine_similarities[0, i]) for i in range(cosine_similarities.shape[1])]
+    results = [(titles[i], cosine_similarities[0, i]) for i in range(cosine_similarities.shape[1])]
     return sorted(results, key=lambda x: x[1], reverse=True)
 
-query = "Chicago"
+# testing example
+query = "University"
 results = search(query)
 for result, similarity in results:
     print(f"Similarity: {similarity}\n{result}")
